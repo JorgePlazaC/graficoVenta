8a8dc63e1ba3f614b0c5cbc12d65dbf6ba2638fe
JorgePlazaC <chocheplaza@hotmail.com>
2023-05-19 21:40:52 -0400

    first commit2

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Linq;
 
 namespace Proyecto1.Pages;
 
@@ -10,6 +12,7 @@ public class IndexModel : PageModel
     public List<Producto> productos;
     public List<Ventum> ventum;
     public List<Producto> productosVendidos;
+    public List<Decimal> sumaTotalPorFecha; 
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
@@ -25,10 +28,20 @@ public class IndexModel : PageModel
         ventum = context.Venta.ToList();
 
         DateTime fechaDeterminada = new DateTime(2023, 5, 20); // Fecha determinada que deseas consultar
+        DateTime fechaInicio = new DateTime(2023, 5, 20);
+        DateTime fechaFin = new DateTime(2023, 5, 31);
 
         productosVendidos = context.VentaProductos
             .Where(vp => vp.Venta.Fecha.Date == fechaDeterminada.Date)
             .Select(vp => vp.Producto)
             .ToList();
+
+        
+        sumaTotalPorFecha = context.VentaProductos
+            .Where(vp => vp.Venta.Fecha >= fechaInicio && vp.Venta.Fecha <= fechaFin)
+            .GroupBy(vp => vp.Venta.Fecha.Date)
+            .Select(g => g.Sum(vp => vp.Precio))
+            .ToList();
+        
     }
 }
